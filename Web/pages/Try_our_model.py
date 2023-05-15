4051159dd5b5539306d9b2320c6de395f0beb46c
NovedaC <123772749+NovedaC@users.noreply.github.com>
2023-05-16 01:11:41 +0800

    added more descriptions

--- Web/pages/Try_our_model.py
+++ Web/pages/Try_our_model.py
@@ -16,8 +16,31 @@ st.write("""
 # MRI Brain Tumor Classifier
 """)
 
-uploaded_file = st.file_uploader("Upload Your image", accept_multiple_files=False)
+st.write("""
+Our application take in a image of brain tumor Magnetic Resonance Imaging (MRI) and classify it with our trained CNN model. We support image of front views, side views, top views, and back views.
+""")
+st.write("""
+### Categories
+There are four possible result: 
+- Pituitary Tumor
+- No Tumor
+- Meningioma Tumor
+- Glioma Tumor
+""")
+
+st.write("""
+### Model Result
+- Training Accuracy: 0.9825783972125436
+
+- Validation Accuracy: 0.8571428571428571
+
+- Testing Accuracy: 0.7055837563451777
+""")
+
+st.header("Upload Your image")
+uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False)
 if uploaded_file is not None:
+    st.header("Result")
     col1, col2 = st.columns([1, 2])
     image = Image.open(uploaded_file)
     with col1:
@@ -25,3 +48,6 @@ if uploaded_file is not None:
     fig = view_classify(image, model)
     with col2:
         st.plotly_chart(fig, use_container_width=True)
+    st.write("""
+    Base on the bar graph, we choose the highest probablility option as our classification.
+    """)
